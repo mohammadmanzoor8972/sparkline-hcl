b073e0145507a5b318a6f326ccf48d09177fa560
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-02-07 00:47:02 +0530

    Implement Sorting and SparkLine

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ class analysis {
 
   constructor(){
     this.currency = {};
+    this.sparkLine = {};
     this.content = document.getElementById("tbody");
     this.url = "ws://localhost:8011/stomp";
-    this.udpateInterval = 3000;
+    this.udpateInterval = 30000; //Update sparkline after 30 seconds.
     this.client = Stomp.client(this.url);
     this.initilizeConnection();
   }
@@ -16,11 +17,17 @@ class analysis {
   initilizeConnection(){
     this.client.connect({}, (resp)=>{
       this.subscribeMessage();
+      this.addEventListeners();
     }, function(error) {
       alert(error.headers.message);
     });
   }
 
+  addEventListeners(){
+    document.querySelector("#changeHandler").addEventListener("change", (ev)=>{this.sortTable(ev.target.value)});
+  }
+
+  
   subscribeMessage(){
     this.client.subscribe("/fx/prices", (message)=>{
       if (message.body) {
@@ -50,26 +57,83 @@ class analysis {
         rowTemplate.push("<td><span id=" + item.name + ">0</span></td>");
         rowTemplate.push("</tr>");
     this.content.innerHTML += rowTemplate.join("");
-
+   
     Object.values(this.currency).map(item=>{
       this.generateSparkLine(item);
     })
   }
 
   updateRow(data){
-    var row = document.querySelector("#"+data.name+"-data");
-      row.children[3].textContent=data.lastChangeBid;
-      row.children[4].textContent=data.lastChangeAsk;
       this.generateSparkLine(data);
   }
 
   generateSparkLine(currencyItem){
-    const el = document.getElementById(currencyItem.name);
-    Sparkline.draw(el, [
-      currencyItem.bestBid,
-      (currencyItem.bestBid + currencyItem.bestAsk) / 2,
-      currencyItem.bestBid
-    ]);
+   let el = document.getElementById(currencyItem.name);
+   if(!this.sparkLine[currencyItem.name]){
+    this.sparkLine[currencyItem.name] = {};
+    this.sparkLine[currencyItem.name]["values"] = []; 
+    this.sparkLine[currencyItem.name]["spark"] = new Sparkline(el)
+   }
+
+   const d = (currencyItem.bestBid + currencyItem.bestAsk) / 2;
+   this.sparkLine[currencyItem.name]["values"].push(d);
+   Sparkline.draw(el, this.sparkLine[currencyItem.name]["values"]);
+  }
+
+  sortTable(n) {
+    //console.log(even)
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable2");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
   }
 }
 
